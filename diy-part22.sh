3ef97d7e5586a87f8807297d8dc41a38dbb27792
Harrylwc Lau <harry.lwc@gmail.com>
2025-06-07 00:57:51 +0800

    modified:   ../../diy-part22.sh 	modified:   Makefile 	new file:   ../../patches/Makefile.rtl8821cu.5.4 	new file:   ../../patches/kernel-5.4

--- diy-part22.sh
+++ diy-part22.sh
@@ -9,3 +9,13 @@ cp -r $GITHUB_WORKSPACE/package/* $GITHUB_WORKSPACE/x-wrt/package/
 ./scripts/feeds update -a               
 ./scripts/feeds install -a -f
 cp config.aarch64.3 .config
+
+rm target/linux/generic/backport-5.4/430-v6.3-ubi*.patch
+rm target/linux/mvebu/patches-5.4/008-net-mvneta-make-tx-buffer-array-agnostic.patch
+cd $GITHUB_WORKSPACE/x-wrt/
+git clone https://github.com/tvheadend/tvheadend.git
+
+cp -r $GITHUB_WORKSPACE/x-wrt/tvheadend/.git $GITHUB_WORKSPACE/x-wrt/package/tvheadend/files/
+rm -r feeds/packages/multimedia/tvheadend
+cp $GITHUB_WORKSPACE/patches/kernel-5.4 $GITHUB_WORKSPACE/x-wrt/include/
+cp $GITHUB_WORKSPACE/patches/Makefile.rtl8821cu.5.4 $GITHUB_WORKSPACE/x-wrt/package/rtl8821cu/Makefile
