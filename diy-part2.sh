8b23a72673b4288669316565eeddcbbb4ad2e922
Harrylwc Lau <harry.lwc@gmail.com>
2025-09-08 02:00:15 +0800

    modified:   diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -27,7 +27,7 @@ cp $GITHUB_WORKSPACE/patches/Makefile.libconfig  $GITHUB_WORKSPACE/x-wrt/package
 cp $GITHUB_WORKSPACE/patches/Makefile.libx264 $GITHUB_WORKSPACE/x-wrt/package/feeds/packages/libx264/Makefile
 cp $GITHUB_WORKSPACE/patches/Makefile.pdnsd-alt $GITHUB_WORKSPACE/x-wrt/package/feeds/small/pdnsd-alt/Makefile
 cp $GITHUB_WORKSPACE/patches/Makefile.pdnsd-alt $GITHUB_WORKSPACE/x-wrt/package/pdnsd-alt/Makefile
-cp $GITHUB_WORKSPACE/patches/Makefile.simple-obfs $GITHUB_WORKSPACE/x-wrt/package/small/simple-obfs/Makefile
+cp $GITHUB_WORKSPACE/patches/Makefile.simple-obfs $GITHUB_WORKSPACE/x-wrt/package/feeds/small/simple-obfs/Makefile
 cp $GITHUB_WORKSPACE/patches/Makefile.simple-obfs $GITHUB_WORKSPACE/x-wrt/package/simple-obfs/Makefile
 
 
