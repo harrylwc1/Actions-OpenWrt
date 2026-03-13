85eb6c4e90353081292ca7f055bc366544e6ac93
Harrylwc Lau <harry.lwc@gmail.com>
2026-03-14 04:03:41 +0800

    modified:   config.aarch64.3

--- diy-part22.sh
+++ diy-part22.sh
@@ -8,6 +8,8 @@ cd x-wrt
 #kernel 213
 git checkout 8444302a92e601a1e05cb8468aaffa140d5a5b80
 
+
+
 cp -r $GITHUB_WORKSPACE/package/* $GITHUB_WORKSPACE/x-wrt/package/
 
 ./scripts/feeds update -a               
@@ -23,3 +25,7 @@ cp -r $GITHUB_WORKSPACE/x-wrt/tvheadend/.git $GITHUB_WORKSPACE/x-wrt/package/tvh
 rm -r feeds/packages/multimedia/tvheadend
 cp $GITHUB_WORKSPACE/patches/kernel-5.4 $GITHUB_WORKSPACE/x-wrt/include/
 cp $GITHUB_WORKSPACE/patches/Makefile.rtl8821cu.5.4 $GITHUB_WORKSPACE/x-wrt/package/rtl8821cu/Makefile
+
+##kernel 5.4.213 need to delete following patches
+rm $GITHUB_WORKSPACE/x-wrt/target/linux/mvebu/patches-5.4/013-net-mvneta-rely-on-page_pool_recycle_direct-in-mvnet.patch
+rm $GITHUB_WORKSPACE/x-wrt/target/linux/mvebu/patches-5.4/009-net-mvneta-add-XDP_TX-support.patch 
