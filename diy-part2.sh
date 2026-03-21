f735af00b15ab723b3cf4b4d59864ef00ee5a4c9
Harrylwc Lau <harry.lwc@gmail.com>
2026-03-21 17:40:42 +0800

    modified:   diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -158,7 +158,11 @@ git apply -R --ignore-space-change --ignore-whitespace reset_user_to_root.patch
 cd $GITHUB_WORKSPACE/x-wrt/feeds/packages/
 cd ../../
 rm -r $GITHUB_WORKSPACE/x-wrt/package/feeds/small/pdnsd-alt
+rm -r $GITHUB_WORKSPACE/x-wrt/package/small/pdnsd-alt
+
 sudo rm -r $GITHUB_WORKSPACE/x-wrt/package/feeds/small/tcping 
+sudo rm -r $GITHUB_WORKSPACE/x-wrt/package/small/tcping
+
 #if [ -e $GITHUB_WORKSPACE/patches/999-Z-0036-dsa-drop-more-bridge-offload.patch ]; then
 #cp $GITHUB_WORKSPACE/patches/999-Z-0036-dsa-drop-more-bridge-offload.patch $GITHUB_WORKSPACE/x-wrt/target/linux/generic/hack-6.12/
 #fi
