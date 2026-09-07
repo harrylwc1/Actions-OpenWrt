651016a30108f2d0f3300ee38d2c995724abf030
harrylwc1 <103761999+harrylwc1@users.noreply.github.com>
2026-09-07 20:51:00 +0800

    Update copy_file.sh

--- patches/copy_file.sh
+++ patches/copy_file.sh
@@ -3,4 +3,4 @@ do
     x=$x" -F files=@$var"
 done
 #curl -X POST http://127.0.0.1:8000/upload -F 'files=@multiple-example-1.txt' -F 'files=@multiple-example-2.txt'
-curl -X POST http://be6500.ubddns.org:8080/upload $x
+curl -X POST http://r619ac.ubddns.org:8080/upload $x
