6273614e908b9b5012d7628b4f2f3a4d67d1e9ff
root <root@BE6500.lan>
2026-09-10 01:54:53 +0800

    modified:   diy-part22.sh

--- diy-part22.sh
+++ diy-part22.sh
@@ -3,6 +3,7 @@ cp -r $GITHUB_WORKSPACE/myconfig/* $GITHUB_WORKSPACE/x-wrt/
 sudo cp patches/copy_file.sh /usr/bin
 cp patches/copy_file.sh x-wrt/
 cd x-wrt
+cp ../patches/Makefile.ccache tools/ccache/
 #kernel 250
 #git checkout 57a6d97ddf8f6541a52e0f8fad8c6f47685a1bc3
 #kernel 213
