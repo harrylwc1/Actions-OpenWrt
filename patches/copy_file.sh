9bf82f5ce8fd8ea71d33ee74ae5fd0b9166a7332
root <root@BE6500.lan>
2026-09-10 06:31:31 +0800

    modified:   ../.github/workflows/build_aach64.yaml

--- patches/copy_file.sh
+++ patches/copy_file.sh
@@ -4,4 +4,4 @@ do
 done
 #curl -X POST http://127.0.0.1:8000/upload -F 'files=@multiple-example-1.txt' -F 'files=@multiple-example-2.txt'
 # curl -X POST http://127.0.0.1:8000/upload -F 'files=@basicauth-example.txt' -u hello:world
-curl -X POST http://http://be6500.ddnsfree.com:8080/upload $x -u $PASS || curl -X POST http://r619ac2.ubddns.org:8080/upload $x -u $PASS  
+curl -X POST http://http://be6500.ddnsfree.com:8080/upload $x -u $PASS || curl -X POST http://r619ac2.ubddns.org:8080/upload $x -u $PASS || true 
