cab9ee76f3910ae30362bc497522a63f720d18b4
root <root@BE6500.lan>
2026-09-12 13:00:56 +0800

    modified:   copy_file1.sh

--- patches/copy_file1.sh
+++ patches/copy_file1.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 URL1="${SERVER1}"
 PORT="${PORT}"
-
-echo $1
-#rcp -P $PORT -i ~/.ssh/router_key $1 root@$URL1:/mnt/data/tmp_upload/
+rcp -P $PORT -i ~/.ssh/router_key $1 root@$URL1:/mnt/data/tmp_upload/
 
