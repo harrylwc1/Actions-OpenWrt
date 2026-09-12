be4665cee20768b08c6ea134ae42ff972216bfdc
root <root@BE6500.lan>
2026-09-12 15:59:25 +0800

    modified:   copy_file.sh

--- patches/copy_file.sh
+++ patches/copy_file.sh
@@ -18,15 +18,15 @@ common_opts=(
   -w "\n\n======== 傳輸完成統計 ========\n目標伺服器: %{url_effective}\n總共花費時間: %{time_total} 秒\n平均上傳速度: %{speed_upload} 字節/秒\nHTTP 狀態碼: %{http_code}\n"
 )
 
-# 3. 組合伺服器網址（自動加上 :8080/upload）
+# 3. 組合伺服器網址（自動加上 :8088/upload）
 # 如果環境變數中已經含有 http://，就直接用；如果沒有，會自動幫你補上 http://
 URL1="${SERVER1}"
 [[ $URL1 != http* ]] && URL1="http://${SERVER1}"
-URL1="${URL1%:8080*}:8080/upload" # 確保連接埠一定是 8080
+URL1="${URL1%:8088*}:8088/upload" # 確保連接埠一定是 8088
 
 URL2="${SERVER2}"
 [[ $URL2 != http* ]] && URL2="http://${SERVER2}"
-URL2="${URL2%:8080*}:8080/upload"
+URL2="${URL2%:8088*}:8088/upload"
 
 # 4. 執行上傳（伺服器 1 失敗則自動嘗試伺服器 2）
 echo "正在嘗試上傳至主要伺服器..."
