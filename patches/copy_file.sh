9a00e0ef4c0b45fb5b440eb7a4c490dda5a7b4c6
harrylwc1 <103761999+harrylwc1@users.noreply.github.com>
2026-09-19 00:33:23 +0800

    Update copy_file.sh

--- patches/copy_file.sh
+++ patches/copy_file.sh
@@ -54,7 +54,7 @@ do_upload() {
     local http_code
 
     http_code=$(curl \
-        --limit-rate 5M \
+        --limit-rate 3M \
         --no-buffer \
         --connect-timeout 10 \
         --max-time 600 \
