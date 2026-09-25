a03fda7e9a6ffe3f2d18e9477a8fe8d9cd6cc55f
root <root@BE6500.lan>
2026-09-26 03:26:51 +0800

    deleted:    ../package/luci-app-jdcloud-reboot/.github/ISSUE_TEMPLATE/config.yml

--- package/luci-app-jdcloud-reboot/luasrc/controller/jdcloud_reboot.lua
+++
@@ -1,7 +0,0 @@
-module("luci.controller.jdcloud_reboot", package.seeall)
-
-function index()
-entry({"admin", "system", "jdcloud_reboot"},
-      cbi("jdcloud_reboot"),
-      _("JDCloud Reboot"), 60).dependent = false
-end
