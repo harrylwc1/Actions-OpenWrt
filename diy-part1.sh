d75fe2a5fef34a384b84a3039d38955501e100ac
harrylwc1 <103761999+harrylwc1@users.noreply.github.com>
2026-09-25 10:29:46 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -64,6 +64,9 @@ git clone https://github.com/kenzok8/small.git $GITHUB_WORKSPACE/x-wrt/package/s
 sed -i '$a src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
 sed -i '$a src-git small https://github.com/kenzok8/small' feeds.conf.default
 git pull
+git revert --no-edit $(git log --grep="use admin as the default" -n 1 --format="%H")
+
+
 git clone https://github.com/jerrykuku/luci-theme-argon package/luci-theme-argon
 git clone https://github.com/muink/luci-app-netspeedtest.git $GITHUB_WORKSPACE/x-wrt/package/luci-app-netspeedtest
 
