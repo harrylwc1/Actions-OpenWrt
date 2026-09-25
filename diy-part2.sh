006fdc783a46064b6df3b41bc8148a72f6dccf8d
harrylwc1 <103761999+harrylwc1@users.noreply.github.com>
2026-09-25 10:30:10 +0800

    Comment out tvheadend git clone command
    Comment out the git clone command for tvheadend repository.

--- diy-part2.sh
+++ diy-part2.sh
@@ -13,7 +13,7 @@ git revert --no-edit $(git log --grep="use admin as the default" -n 1 --format="
 ‎cat   $GITHUB_WORKSPACE/x-wrt/package/base-files/files/etc/shadow‎|grep admin
 #cp $GITHUB_WORKSPACE/patches/Makefile.scons $GITHUB_WORKSPACE/x-wrt/package/feeds/packages/scons/Makefile
 cd $GITHUB_WORKSPACE/x-wrt/
-git clone https://github.com/tvheadend/tvheadend.git
+#git clone https://github.com/tvheadend/tvheadend.git
 
 cp -r $GITHUB_WORKSPACE/x-wrt/tvheadend/.git $GITHUB_WORKSPACE/x-wrt/package/tvheadend/files/
 rm -r feeds/packages/multimedia/tvheadend
